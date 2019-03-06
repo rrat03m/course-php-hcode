31f7088f2fd2bf1b70a425c1f8e31e445324358c
Rafael <rrat03m@gmail.com>
2019-03-06 20:58:07 -0300

    Variaveis pre-definidas

---
+++ variaveis-pre-definidas/variaveis_globais.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = $_GET["nome"];
+$numero = (int)$_GET["idade"];
+
+// var_dump($nome);
+// var_dump($numero);
+
+$ip = $_SERVER["REMOTE_ADDR"];
+
+$nome_script = $_SERVER["SCRIPT_NAME"];
+
+// echo($ip);
+
+// echo($nome_script);
+
+?>
