e513a2c2c23ac6f96bcace4f4ce71f9b82c1046a
Rafael <rrat03m@gmail.com>
2019-03-18 15:49:37 -0300

    lacos

---
+++ lacos/foreach/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$meses = [
+
+	"Janeiro", "Fevereiro", "Março",
+
+	"Abril" , "Maio", "Junho",
+
+	"Julho", "Agosto", "Setembro",
+
+	"Outubro", "Novembro", "Dezembro"
+
+];
+
+
+foreach ($meses as $index => $mes) {
+	echo "Indice: ".$index."<br>";
+	echo "Mês é ".$mes."<br><br>";
+
+}
+
+
+?>
---
+++ lacos/while/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$condicao = true;
+
+while ($condicao) {
+
+	$numero = rand(1, 10);
+
+	if ($numero === 3){
+
+		echo "Três";
+		$condicao = false;
+
+	}else{
+
+		echo $numero." ";
+
+	}
+}
+
+
+?>
